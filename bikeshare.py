4b3719967a030175f719569a200ccb0957b7f4c8
narcisseisaker <narcisseisaker@gmail.com>
2023-10-30 06:43:47 +0100

    Update day of week document string of get_filters method of bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -13,7 +13,7 @@ def get_filters():
     Returns:
         (str) city - city's name to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - day of week's name to filter by, or "all" to apply no day filter
+        (str) day - name of day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!\n')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
